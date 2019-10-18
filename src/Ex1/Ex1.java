53456933e83a27cbc3a12b58554936bfbb671c9c
2153749860 <tiagorocha1515@gmail.com>
2019-10-18 13:54:45 +0100

    Added interface

--- src/Ex1/Ex1.java
+++ src/Ex1/Ex1.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ficha3;
+package Ex1;
 
 /**
  *
  * @author tiago
  */
-public class Ficha3 {
+public class Ex1 {
 
     /**
      * @param args the command line arguments
